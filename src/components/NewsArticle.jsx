150a60ab025bfd27cc763bcd3dc5d8f564b01c52
Laiba Kamran <laiba.kamran1010@gmail.com>
2023-08-17 18:39:35 +0500

    add design for feaured article

--- src/components/NewsArticle.jsx
+++ src/components/NewsArticle.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import './NewsArticle.css';
 
-const NewsArticle = ({ article }) => {
+const NewsArticle = ({ article, isFirstArticle }) => {
+
+  if (!article || !article.url) {
+    return <p>Error: Invalid article data.</p>;
+  }
+
   const { url, urlToImage, publishedAt, title, description } = article;
 
   const openArticleInNewTab = () => {
@@ -11,11 +16,15 @@ const NewsArticle = ({ article }) => {
   };
 
   return (
-    <div className="news-article" onClick={openArticleInNewTab}>
-      <img src={urlToImage} alt={title} />
+    <div className={` ${isFirstArticle ? 'first-article' : 'news-article'}`} onClick={openArticleInNewTab}>
+      <div className={` ${isFirstArticle ? 'first-article-img' : 'img-container'}`}>
+        <img src={urlToImage} alt={title} />
+        </div>
+      <div className={` ${isFirstArticle ? 'first-article-content' : 'content'}`}>
       <p className="published-date">{new Date(publishedAt).toLocaleDateString()}</p>
       <h3 className="article-title">{title}</h3>
       <p className="article-description">{description}</p>
+      </div>
     </div>
   );
 };
--- src/components/NewsGrid.jsx
+++ src/components/NewsGrid.jsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NewsArticle from './NewsArticle';
 import './NewsGrid.css';
 
 const NewsGrid = ({ news }) => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const [firstArticle, ...restArticles] = news; // Separate the first article from the rest
+
   return (
-    <div className="news-grid">
-      {news.map((newsItem, index) => (
-        <NewsArticle key={index} article={newsItem} />
-      ))}
-    </div>
+    <>
+      {screenWidth < 768 ? (
+        <div className='news-grid'>
+        {news.map((newsItem, index) => (
+          <NewsArticle key={index} article={newsItem} isFirstArticle={false}/>
+        ))}
+      </div>   
+      ) : (
+        <div className='news-grid'>
+          <div className="big-article">
+            <NewsArticle article={firstArticle} isFirstArticle={true} />
+          </div>
+          <div className="small-articles">
+            {restArticles.map((newsItem, index) => (
+              <NewsArticle key={index} article={newsItem} isFirstArticle={false} />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
