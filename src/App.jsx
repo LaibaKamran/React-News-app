c173d3578269fe7a0b10863d5f8f929597ccf9a8
Laiba Kamran <laiba.kamran1010@gmail.com>
2023-08-20 01:06:25 +0500

    add react router to access different categories news from navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,25 @@ import { fetchNews, selectNews } from './features/news/newsSlice';
 import NewsGrid from './components/NewsGrid';
 import Navbar from './components/Navbar';
 import Pagination from './components/Pagination';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import TechPage from './components/TechPage';
+import BusinessPage from './components/BusinessPage';
+import SportsPage from './components/SportsPage';
 
 function App() {
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Navbar />,
-      children: [
-        {
-          path: "/",
-          element: <HomePage/>,
-        },
-      ],
-    },
-  ]);
   
 
   return (
     <div className="App">
-      <RouterProvider router={router} />
+      <Navbar />
+       <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/tech" element={<TechPage />} />
+          <Route path="/business" element={<BusinessPage />} />
+          <Route path="/sports" element={<SportsPage />} />
+       </Routes>
     </div>
   );
 }
