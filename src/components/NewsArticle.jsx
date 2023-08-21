3ddaca1835c844278f270b96c6b74fb8a80a915a
Laiba Kamran <laiba.kamran1010@gmail.com>
2023-08-21 16:51:29 +0500

    apply words limit for description so all articles have same size

--- src/components/NewsArticle.jsx
+++ src/components/NewsArticle.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import './NewsArticle.css';
 
-const NewsArticle = ({ article, isFirstArticle }) => {
-
+const NewsArticle = ({ article, isFirstArticle, isDesktop }) => {
   if (!article || !article.url) {
     return <p>Error: Invalid article data.</p>;
   }
@@ -15,15 +14,24 @@ const NewsArticle = ({ article, isFirstArticle }) => {
     }
   };
 
+  const getDescription = () => {
+    if (isFirstArticle || isDesktop) {
+      return description;
+    } else {
+      const words = description.split(' ');
+      return words.slice(0, 20).join(' ') + '...'; // Display first 30 words and add ellipsis
+    }
+  };
+
   return (
     <div className={` ${isFirstArticle ? 'first-article' : 'news-article'}`} onClick={openArticleInNewTab}>
       <div className={` ${isFirstArticle ? 'first-article-img' : 'img-container'}`}>
         <img src={urlToImage} alt={title} />
-        </div>
+      </div>
       <div className={` ${isFirstArticle ? 'first-article-content' : 'content'}`}>
-      <p className="published-date">{new Date(publishedAt).toLocaleDateString()}</p>
-      <h3 className="article-title">{title}</h3>
-      <p className="article-description">{description}</p>
+        <p className="published-date">{new Date(publishedAt).toLocaleDateString()}</p>
+        <h3 className="article-title">{title}</h3>
+        <p className="article-description">{getDescription()}</p>
       </div>
     </div>
   );
